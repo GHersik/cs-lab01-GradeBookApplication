30592ebc4845b24c3d82eb44fed162d158d727d5
GHersik <80617931+GHersik@users.noreply.github.com>
2022-03-15 18:21:54 +0100

    Update to all the constructors of each gradebook class to accept bool as the second parameter along with an update to the CreateCommand method in StartingUserInterface to accept one more value of true or false.

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,21 +34,22 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 3)
+            if (parts.Length != 4)
             {
                 Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
             var name = parts[1];
+            bool IsWeighted = bool.Parse(parts[3]);
             if (parts[2] == "standard")
             {
-                StandardGradeBook gradeBook = new StandardGradeBook(name);
+                StandardGradeBook gradeBook = new StandardGradeBook(name, IsWeighted);
                 Console.WriteLine("Created gradebook {0}.", name);
                 GradeBookUserInterface.CommandLoop(gradeBook);
             }
             else if (parts[2] == "ranked")
             {
-                RankedGradeBook rankedGradeBook = new RankedGradeBook(name);
+                RankedGradeBook rankedGradeBook = new RankedGradeBook(name, IsWeighted);
                 Console.WriteLine("Created gradebook {0}.", name);
                 GradeBookUserInterface.CommandLoop(rankedGradeBook);
             }
@@ -57,9 +58,6 @@ namespace GradeBook.UserInterfaces
                 Console.WriteLine($"{parts[2]} is not a supported type of gradebook, please try again");
                 return;
             }
-            //BaseGradeBook gradeBook = new BaseGradeBook(name);
-            //Console.WriteLine("Created gradebook {0}.", name);
-            //GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
         public static void LoadCommand(string command)
