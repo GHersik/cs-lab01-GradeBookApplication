a61fcbad889c347ed67dc9e05177870ce1118a69
GHersik <80617931+GHersik@users.noreply.github.com>
2022-03-15 17:15:34 +0100

    Updated classes to public.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,11 +4,21 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return base.GetLetterGrade(averageGrade);
         }
     }
 }
